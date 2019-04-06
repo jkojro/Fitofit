09d0de5a4f716354fd618668763a5886a379330e
jkojro <janusz.kojro@sparksoluitons.co>
2019-04-06 17:15:00 +0200

    Add new_walk page feature spec pseudocode

---
+++ spec/features/new_walk_page_spec.rb
@@ -0,0 +1,11 @@
+# - go to ../walk/new
+#
+# - fill in start location with 'Plac Europejski 2, Warszawa, Polska'
+#
+# - fill in end location with 'Poznańska 121, Warszawa, Polska'
+#
+# - click 'Submit'
+#
+# - it should redirect to index page
+#
+# - it should display 'Your last walk was 1.99 km long'
