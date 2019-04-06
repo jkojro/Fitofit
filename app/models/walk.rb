aecdf9fb6ce8b34f35294a0c3729b56b1d5096b1
jkojro <janusz.kojro@sparksoluitons.co>
2019-04-06 15:58:53 +0200

    Add Walk model validation

--- app/models/walk.rb
+++ app/models/walk.rb
@@ -1,2 +1,5 @@
 class Walk < ApplicationRecord
+  validates :start_location, :end_location, :distance, presence: true
+  validates_format_of :start_location, :end_location,
+    with: /\A^[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]+( +[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]+)* \d+, [A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]+( +[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]+)*, [A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]+( +[A-Za-zżźćńółęąśŻŹĆĄŚĘŁÓŃ]+)*\z/
 end
--- spec/models/walk_spec.rb
+++ spec/models/walk_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Walk, type: :model do
 
     context "location format" do
       it { should allow_value("Plac Europejski 2, Warszawa, Polska").for(:start_location) }
-      it { should allow_value("Puławska Ęłźżcz-polskie znaki 2, Warszawa, Polska").for(:end_location) }
+      it { should allow_value("Puławska Ęłźżcz polskie znaki 2, Warszawa, Polska").for(:end_location) }
       it { should_not allow_value("Plac Europejski, Warszawa, Polska").for(:start_location) }
       it { should_not allow_value("Plac Europejski 3, Warszawa").for(:end_location) }
     end
