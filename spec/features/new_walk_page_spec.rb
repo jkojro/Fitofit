79631bb34aedf3ec4e4f3246f20ab483c715e6de
jkojro <janusz.kojro@sparksoluitons.co>
2019-04-06 17:19:59 +0200

    Add new_walk feature spec

--- spec/features/new_walk_page_spec.rb
+++ spec/features/new_walk_page_spec.rb
@@ -1,11 +1,37 @@
-# - go to ../walk/new
-#
-# - fill in start location with 'Plac Europejski 2, Warszawa, Polska'
-#
-# - fill in end location with 'Poznańska 121, Warszawa, Polska'
-#
-# - click 'Submit'
-#
-# - it should redirect to index page
-#
-# - it should display 'Your last walk was 1.99 km long'
+require 'rails_helper'
+
+describe WalksController, type: :feature do
+  describe '#new' do
+    scenario 'new page returns 200 status' do
+      visit new_walk_path
+
+      expect(page.status_code).to eq 200
+      expect(page.current_path).to eq new_walk_path
+    end
+  end
+
+  describe '#create' do
+    scenario 'new walk is submited' do
+      visit new_walk_path
+
+      fill_in 'Start point', with: 'Plac Europejski 2, Warszawa, Polska'
+      fill_in 'End point', with: 'Poznańska 121, Warszawa, Polska'
+
+      click_button 'Submit'
+
+      expect(page).to have_content('On your last walk you made distance of 1.99 km')
+    end
+
+    scenario 'invalid data is submited' do
+      visit new_walk_path
+
+      fill_in 'Start point', with: 'No city 2, Polska'
+      fill_in 'End point', with: 'No street number, Warszawa, Polska'
+
+      click_button 'Submit'
+
+      expect(page).to have_content('Start location is invalid')
+      expect(page).to have_content('End location is invalid')
+    end
+  end
+end
