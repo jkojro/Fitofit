6b72465f7ac08c3daac86e91a8307be218a76491
jkojro <janusz.kojro@sparksoluitons.co>
2019-04-06 17:44:26 +0200

    Add WalksController spec

---
+++ spec/controllers/walks_controller_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+describe WalksController, type: :controller do
+  describe 'GET #new' do
+    let(:action) { get :new }
+    it 'renders the :new view' do
+      action
+      expect(response.status).to eq(200)
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    let(:action) { post :create, params: { walk: params } }
+
+    context 'with valid attributes' do
+      let(:params) { { start_location: 'Dąbrowskiego 15, Warszawa, Polska', end_location: 'Białostoska 1, Warszawa, Polska' } }
+
+      it 'response with status 200' do
+        expect(response.status).to eq(200)
+      end
+
+      it 'creates walk' do
+        expect { action }.to change { Walk.count }.by(1)
+      end
+    end
+
+    context 'with invalid attributes' do
+      let!(:params) { { start_location: 'Dąbrowskiego 15 55, Warszawa, Polska', end_location: 'Białostoska 1, Warszawa' } }
+
+      it 'response with status 200' do
+        expect(response.status).to eq(200)
+      end
+
+      it 'doesnt create walk with valid attributes' do
+        expect { action }.to change { Walk.count }.by(0)
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    let(:walk) { FactoryBot.create(:walk) }
+    let(:action) { get :show, params: { id: walk.id } }
+
+    it 'renders the :show view' do
+      action
+      expect(response.status).to eq(200)
+      expect(response).to render_template :show
+    end
+  end
+end
