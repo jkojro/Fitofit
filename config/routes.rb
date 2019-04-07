7cecff6b2cbe4b787725a76d3aa948eec5fcd7d8
jkojro <janusz.kojro@sparksoluitons.co>
2019-04-07 17:48:36 +0200

    Add mothly statistics action and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :walks, only: [:new, :create, :show]
+  resources :walks, only: [:new, :create, :show, :index]
   root 'walks#new'
 end
